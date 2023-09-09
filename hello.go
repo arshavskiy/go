28f3084a3a9d7cd489b7746a062d73e0d0cc8c7d
Pavel <arshavsky.pasha@gmail.com>
2023-09-09 12:44:56 +0300

    static server

--- hello.go
+++ hello.go
@@ -12,8 +12,11 @@ func main() {
 	fmt.Println(quote.Opt())
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Welcome to my website!")
 	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.ListenAndServe(":80", nil)
 }
