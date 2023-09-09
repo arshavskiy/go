3d36f039b121ebd1bd87acb1275167a442c102f6
Pavel <arshavsky.pasha@gmail.com>
2023-09-09 12:21:24 +0300

    init http

--- hello.go
+++ hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"rsc.io/quote"
 )
@@ -9,4 +10,10 @@ import (
 func main() {
 	fmt.Println(quote.Go())
 	fmt.Println(quote.Opt())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":80", nil)
 }
